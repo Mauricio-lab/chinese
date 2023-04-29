58cf21b386fa34b67d3be06f84d69d88f231d889
mlicul@yahoo.it <mlicul@yahoo.it>
2023-04-29 15:57:40 +0200

    first commit

--- render-build.sh
+++ render-build.sh
@@ -3,11 +3,11 @@
 set -o errexit STORAGE_DIR=/opt/render/project/.render 
 if [[ ! -d $STORAGE_DIR/chrome ]]; then 
     echo "...Downloading Chrome" 
-    sudo mkdir -p $STORAGE_DIR/chrome 
-    sudo cd $STORAGE_DIR/chrome 
+    #mkdir -p $STORAGE_DIR/chrome 
+    #sudo cd $STORAGE_DIR/chrome 
     wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
-    sudo dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome 
-    sudo rm ./google-chrome-stable_current_amd64.deb 
+    dpkg -x ./google-chrome-stable_current_amd64.deb 
+    rm ./google-chrome-stable_current_amd64.deb 
     cd $HOME/project/src 
     # Make sure we return to where we were
     else 
