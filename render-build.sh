75ee20a637f1b4f7a13f29cfb34d6609d39d3263
mlicul@yahoo.it <mlicul@yahoo.it>
2023-04-30 09:02:49 +0200

    first commit

--- render-build.sh
+++ render-build.sh
@@ -10,8 +10,11 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  wget https://chromedriver.storage.googleapis.com/95.0.4638.54/chromedriver_linux64.zip
+  #wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  #dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  unzip chromedriver_linux64.zip
+  ./chromedriver
   cd opt
   cd google
   cd chrome  
