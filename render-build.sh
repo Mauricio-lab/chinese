bd7375f44359ab2fdfdcef3109899d700faabf93
mlicul@yahoo.it <mlicul@yahoo.it>
2023-05-02 08:49:13 +0200

    first commit

--- render-build.sh
+++ render-build.sh
@@ -10,9 +10,9 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   unzip chromedriver_linux64.zip
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
+  #wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  #dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  #rm ./google-chrome-stable_current_amd64.deb
   cd $HOME/project/src # Make sure we return to where we were
 else
   echo "...Using Chrome from cache"
