1dfed49a654c3edc146b22c4678c2015dc7bc6c2
mlicul@yahoo.it <mlicul@yahoo.it>
2023-05-02 09:34:06 +0200

    first commit

--- render-build.sh
+++ render-build.sh
@@ -6,16 +6,24 @@ STORAGE_DIR=/opt/render/project/.render
 
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
-  wget https://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip
-  unzip chromedriver_linux64.zip
+  #wget https://chromedriver.storage.googleapis.com/112.0.5615.49/chromedriver_linux64.zip
+  #unzip chromedriver_linux64.zip
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  #wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  #dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  #rm ./google-chrome-stable_current_amd64.deb
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  echo "________________________________________________"
+  echo "________________________________________________"
+  echo "________________________________________________"
+  echo "________________________________________________"
+  ./google-chrome
+  echo "________________________________________________"
+  echo "________________________________________________"
+  echo "________________________________________________"
+  echo "________________________________________________"
   cd $HOME/project/src # Make sure we return to where we were
 else
-  echo "...Using Chrome from cache________________________"
 fi
 
 # be sure to add Chromes location to the PATH as part of your Start Command
