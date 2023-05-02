e4ab81e84c4e2aa1190184a4da27ea72789a53d5
mlicul@yahoo.it <mlicul@yahoo.it>
2023-05-02 09:02:08 +0200

    first commit

--- dataflip.py
+++ dataflip.py
@@ -34,7 +34,7 @@ options.add_argument('--disable-dev-shm-usage')
 chrome_options = webdriver.ChromeOptions()
 #driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
 #driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
-driver = webdriver.Chrome("/opt/render/project/.render/chromedriver",options=options)
+driver = webdriver.Chrome("./chromedriver",options=options)
 #service = Service(ChromeDriverManager().install())
 #driver = webdriver.Chrome(options=options)
 
